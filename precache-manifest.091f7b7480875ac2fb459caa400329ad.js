2463ba2bc423d3a744fb74aa454901e2adc7bfcd
MatthewViens <matthew.j.viens@gmail.com>
2019-05-27 14:40:46 -0700

    Updates

---
+++ precache-manifest.091f7b7480875ac2fb459caa400329ad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c0d1b7db56b22ff60fb7815386da293",
+    "url": "/React-Todo/index.html"
+  },
+  {
+    "revision": "d965cbbaa23f4eb67d09",
+    "url": "/React-Todo/static/css/main.b785d114.chunk.css"
+  },
+  {
+    "revision": "40a77a34fcd573ea70c4",
+    "url": "/React-Todo/static/js/2.7b2088d0.chunk.js"
+  },
+  {
+    "revision": "d965cbbaa23f4eb67d09",
+    "url": "/React-Todo/static/js/main.c08ebdab.chunk.js"
+  },
+  {
+    "revision": "8cccfb14f8793be292ed",
+    "url": "/React-Todo/static/js/runtime~main.58a39a8a.js"
+  }
+]);
